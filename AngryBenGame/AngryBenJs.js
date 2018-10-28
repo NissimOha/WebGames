c5bb8eddf5d4b2e1da7e6367d06f21b81066a76d
nissim ohayon <nissimmoha@gmail.com>
2018-10-28 15:46:55 +0200

    Adding Games
    Simple games that represent the using of Client-Side-Web games by html, js, jquery, css and animations,

---
+++ AngryBenGame/AngryBenJs.js
@@ -0,0 +1,96 @@
+﻿var tablePlace = document.getElementById("tablePlace");
+var levels = ["קל", "בינוני", "קשה"];
+var currentLevel = 0;
+var tableLevels = [3, 5, 10];
+var hasTable = false;
+var benShow = document.createElement("img");
+var benHit = document.createElement("img");
+var numberOfhits = 0;
+var numberOfshowed = -1;
+var steps = [
+    { time: ((Math.random() * 0.5) + 1), cssClass: "benShowStyle" },
+    { time: ((Math.random() * 0.75) + 0.25), cssClass: "benHide" }];
+var currentStep = 0;
+
+function levleChange() {
+    var level = document.getElementsByTagName("select")[0].value;
+    if (level == levels[0]) currentLevel = 0;
+    if (level == levels[1]) currentLevel = 1;
+    if (level == levels[2]) currentLevel = 2;
+}
+
+function reselAll() {
+    if (hasTable) {
+        tablePlace.removeChild(document.getElementById("table"));
+    }
+    setVisible("visible", "scoreVisible", "scoreHidden");
+    numberOfshowed = -1;
+    numberOfhits = 0;
+}
+
+function setVisible(isVisible, fromClass, toClass) {
+    document.getElementById("btn").style.visibility = isVisible;
+    document.getElementById("spanLevel").style.visibility = isVisible;
+    document.getElementById("select").style.visibility = isVisible;
+    var score = document.getElementsByClassName(fromClass);
+    var length = score.length;
+    for (var i = 0; i < length; i++) {
+        score[0].className = toClass;
+    }
+}
+
+function setScore() {
+    benShow.removeEventListener("click", setScore);
+    benShow.className = "benHitStyle";
+    document.getElementById("numOfHits").innerHTML = ++numberOfhits;
+}
+
+function endGame() {
+    alert("Game-Over!!!     Score: " + document.getElementById("numOfHits").innerHTML);
+    reselAll();
+}
+
+function Game() {
+    if (currentStep == 0)
+        document.getElementById("numOfFails").innerHTML = ++numberOfshowed - numberOfhits;
+    if (numberOfshowed - numberOfhits == 3) {
+        endGame();
+        return;
+    }
+
+    var row = Math.floor(Math.random() * tableLevels[currentLevel]);
+    var col = Math.floor(Math.random() * tableLevels[currentLevel]);
+    var cell = document.getElementById(String(row) + String(col));
+
+    benShow.className = steps[currentStep].cssClass;
+    benShow.addEventListener("click", setScore);
+    cell.appendChild(benShow);
+
+    setTimeout(Game, steps[currentStep].time * 1000);
+    currentStep = currentStep < steps.length - 1 ? currentStep + 1 : 0;
+}
+
+function buildGame() {
+    setVisible("hidden", "scoreHidden", "scoreVisible");
+    document.getElementById("numOfHits").innerHTML = numberOfhits;
+    document.getElementById("numOfFails").innerHTML = numberOfshowed - numberOfhits;
+
+    var table = document.createElement("table");
+    table.id = "table";
+
+    for (var i = 0; i < tableLevels[currentLevel]; i++) {
+        var tr = document.createElement("tr");
+        for (var j = 0; j < tableLevels[currentLevel]; j++) {
+            var td = document.createElement("td");
+            var div = document.createElement("div");
+            div.id = String(i) + String(j);
+            div.className = "hole";
+            td.appendChild(div);
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+    }
+    hasTable = true;
+    tablePlace.appendChild(table);
+    Game();
+}
---
+++ SpaceStarGmae/SpaceStarMainGame.js
@@ -0,0 +1,185 @@
+﻿$(document).ready(function () {
+
+    var $panel;
+    var $myShip;
+    var currentX;
+    var currentY;
+    var leftBorder;
+    var rightBorder;
+    var enemyShipLimit;
+    var enemyShipCounter;
+    var totalScore;
+    var enemyType;
+    var enemyShipShowed;
+    var enemyShipHits;
+    var enemyShipPoints;
+
+    function init() {
+        resetGame();
+
+        $panel = $("<div>");
+        $myShip = $("<img>");
+        $panel.attr("class", "w3-transparent myContainer");
+        $myShip.attr("class", "shipSizeStyle myShipStyle");
+        $("body").on("keydown", keyDownEventListener);
+
+        ($panel).append($myShip);
+        $("body").prepend($panel);
+        dropEnemy();
+    }
+
+    function resetGame() {
+        currentX = 800;
+        leftBorder = 0;
+        rightBorder = 1620;
+        enemyShipShowed = [0, 0, 0];
+        enemyShipHits = [0, 0, 0];
+        enemyShipPoints = [0, 0, 0];
+        enemyShipLimit = 100;
+        enemyShipCounter = 0;
+        totalScore = 0;
+        enemyShipPoint = [10, 25, 50];
+        enemyType = { enemyShip: 0, enemyCommanderShip: 1, enemyMotherFuckerShip: 2 };
+    }
+
+    function dropEnemy() {
+        if (enemyShipCounter == enemyShipLimit) {
+            gameOver();
+            return;
+        }
+        var timeBetweenDrop = Math.random() * 4;
+        var ememyType = Math.random();
+        setTimeout(dropEnemy, timeBetweenDrop * 1000);
+
+        if (ememyType <= 0.7) {
+            var $myEmemy = $("<img>").attr("class", "enemyStyle enemyShipStyle").attr("id", "enemyShip");
+            enemyShipShowed[enemyType.enemyShip]++;
+            updateTable();
+            drop($myEmemy);
+        }
+        else if (ememyType <= 0.9) {
+            var $myEmemy = $("<img>").attr("class", "enemyStyle enemyCommanderShipStyle").attr("id", "enemyCommanderShip");
+            enemyShipShowed[enemyType.enemyCommanderShip]++;
+            updateTable();
+            drop($myEmemy);
+        }
+        else {
+            var $myEmemy = $("<img>").attr("class", "enemyStyle enemyMotherFuckerShipStyle").attr("id", "enemyMotherFuckerShip");
+            enemyShipShowed[enemyType.enemyMotherFuckerShip]++;
+            updateTable();
+            drop($myEmemy);
+        }
+        enemyShipCounter++;
+    }
+
+    function drop($myEmemy) {
+        var enemyPos = Math.random() * 1450;
+        $myEmemy.css("left", enemyPos).css("top", 0);
+        $panel.append($myEmemy);
+        $myEmemy.animate({
+            top: "90%"
+        }, {
+                duration: 5000,
+                complete: function () {
+                    $myEmemy.remove();
+                },
+                progress: function (o, p, t) {
+                    let $enemy = $(this);
+
+                    $(".rocketStyle").each(function () {
+                        checkIfHit($(this), $enemy);
+                    })
+                }
+            })
+    }
+
+    function shot() {
+        var $myRocket = $("<img>").attr("class", "rocketStyle");
+        $myRocket.css("left", currentX).css("top", 750);
+        $panel.append($myRocket);
+        $myRocket.animate({
+            top: "0%"
+        },
+            {
+                duration: 4000,
+                complete: function () {
+                    $myRocket.remove();
+                },
+                progress: function (o, p, t) {
+                    let $rocket = $(this);
+
+                    $(".enemyStyle").each(function () {
+                        checkIfHit($rocket, $(this));
+                    })
+                }
+            });
+    }
+
+    function checkIfHit(rocket, enemy) {
+        if (rocket.offset().top <= enemy.offset().top + 50 && rocket.offset().top >= enemy.offset().top
+            && rocket.offset().left >= enemy.offset().left && rocket.offset().left <= enemy.offset().left + 200) {
+            rocket.remove();
+            enemy.remove();
+            if (enemy.attr("id") == "enemyShip") {
+                enemyShipHits[enemyType.enemyShip]++;
+                enemyShipPoints[enemyType.enemyShip] += enemyShipPoint[enemyType.enemyShip];
+            }
+            else if (enemy.attr("id") == "enemyCommanderShip") {
+                enemyShipHits[enemyType.enemyCommanderShip]++;
+                enemyShipPoints[enemyType.enemyCommanderShip] += enemyShipPoint[enemyType.enemyCommanderShip];
+            }
+            else {
+                enemyShipHits[enemyType.enemyMotherFuckerShip]++;
+                enemyShipPoints[enemyType.enemyMotherFuckerShip] += enemyShipPoint[enemyType.enemyMotherFuckerShip];
+            }
+            updateTable();
+        }
+    }
+
+    function keyDownEventListener() {
+        if (window.event.keyCode == 37) moveLeft();
+        if (window.event.keyCode == 38) shot();
+        if (window.event.keyCode == 39) moveRight();
+    }
+
+    function moveRight() {
+        if (currentX + 20 <= rightBorder) {
+            $myShip.css("left", currentX + 20 + "px");
+            currentX += 20;
+        }
+    }
+
+    function moveLeft() {
+        if (currentX - 20 >= leftBorder) {
+            $myShip.css("left", currentX - 20 + "px");
+            currentX -= 20;
+        }
+    }
+
+    function updateTable() {
+        $("#enemyShipHits").html(enemyShipHits[enemyType.enemyShip] + "/" + enemyShipShowed[enemyType.enemyShip]);
+        $("#enemyCommanderShipHits").html(enemyShipHits[enemyType.enemyCommanderShip] + "/" + enemyShipShowed[enemyType.enemyCommanderShip]);
+        $("#enemyMotherFuckerShipHits").html(enemyShipHits[enemyType.enemyMotherFuckerShip] + "/" + enemyShipShowed[enemyType.enemyMotherFuckerShip]);
+        $("#enemyTotalShipHits").html(
+            (enemyShipHits[enemyType.enemyShip] + enemyShipHits[enemyType.enemyCommanderShip] + enemyShipHits[enemyType.enemyMotherFuckerShip])
+            + "/" +
+            (enemyShipShowed[enemyType.enemyShip] + enemyShipShowed[enemyType.enemyCommanderShip] + enemyShipShowed[enemyType.enemyMotherFuckerShip]));
+
+        $("#enemyShipPoints").html(enemyShipPoints[enemyType.enemyShip]);
+        $("#enemyCommanderShipPoints").html(enemyShipPoints[enemyType.enemyCommanderShip]);
+        $("#enemyMotherFuckerShipPoints").html(enemyShipPoints[enemyType.enemyMotherFuckerShip]);
+        totalScore = enemyShipPoints[enemyType.enemyShip] + enemyShipPoints[enemyType.enemyCommanderShip] + enemyShipPoints[enemyType.enemyMotherFuckerShip];
+        $("#enemyTotalShipPoints").html(totalScore);
+    }
+
+    function gameOver() {
+        alert("Game-Over! Your Score: " + totalScore + " Points." + " Press OK to start over.");
+        $myShip.remove();
+        $panel.remove();
+        $("body").off("keydown", keyDownEventListener);
+        init();
+    }
+
+    init();
+
+})
